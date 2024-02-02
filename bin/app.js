158f16dfe7d8e8e765ae80984e51c35ed5a4e713
Joffrey TERRINE <joffreyterrine@protonmail.com>
2024-02-02 16:36:26 +0100

    travailjs

--- bin/app.js
+++ bin/app.js
@@ -1,13 +1,17 @@
-const apiURL = "http://127.0.0.1:8000/"
-    
-    document.addEventListener("DOMContentLoaded",function(){
-        fetch(apiURL)
-        .then(reponse => response.json())
-        .then(data => {
-            //mettre les élements avec document.getElementById
-            document.getElementById('video-card').innerHTML = '';
-        })
-        .catch(error => {
-            console.error("Erreur lors de la récupération de l'API",error)
-        });
+function displayResult(url){
+    fetch(url).then(function(reponse){
+        return reponse.json().then(
+            function(data){
+                console.log(data.results[0]["image_url"])
+                return data;
+            })
+    })
+    .catch(function(error){
+        console.log(error.message);
     });
+}
+
+
+let url = "http://127.0.0.1:8000/api/v1/titles/?sort_by=-votes,-imdb_score";
+
+displayResult(url);
