295d2a992796eea8a79e9e3805c12c5bae68809d
MentalDeFer972 <joffreyterrine@protonmail.com>
2024-02-04 12:40:52 +0100

    ajoutcoodejs

--- bin/app.js
+++ bin/app.js
@@ -1,17 +1,26 @@
-function displayResult(url){
-    fetch(url).then(function(reponse){
-        return reponse.json().then(
-            function(data){
-                console.log(data.results[0]["image_url"])
-                return data;
+var url = "http://127.0.0.1:8000/api/v1/titles/";
+let triparvotes = "?sort_by=-votes,-imdb_score";
+
+function ajouterMeilleursFilmsSite(){
+
+    let couverture = document.getElementsByClassName('couverture');
+    let titre = document.getElementsByClassName('titre')
+    let filmDescription = docuement.getElementsByClassName('description');
+    let boutonInfo = document.getElementsByClassName('boutoninfo');
+
+    fetch(url+triparvotes)
+    .then(reponse=>reponse.json())
+    .then(data =>{
+                couverture.innerHTML = "<img src="+data['results'][0]['titre']+">"
+                titre.innerHTML = data['results'][0]['title'];
+                boutonInfo.setAttribute("onClick");
+                fetch(data['results'][0]['url'])
+                    .then(reponse => response.json())
+                    .then(data =>
+                            {
+                                filmDescription.innerHTML = data['description']})
             })
-    })
-    .catch(function(error){
-        console.log(error.message);
+    .catch(error =>{ 
+        console.log(error.message)
     });
 }
-
-
-let url = "http://127.0.0.1:8000/api/v1/titles/?sort_by=-votes,-imdb_score";
-
-displayResult(url);
