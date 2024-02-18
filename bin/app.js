6e47a6cce06b285f6561afdbfe872600b78b537b
MentalDeFer972 <joffreyterrine@protonmail.com>
2024-02-18 20:04:30 +0100

    ajoutcodecss

--- bin/app.js
+++ bin/app.js
@@ -1,9 +1,7 @@
 var imageFilm = document.getElementById('couverture');
 var meilleurFilm = document.getElementById('meilleur-film');
-var carousel = document.getElementById('carousel')
-var carouselInner = document.getElementById('carousel-inner')
-var prevBtn = document.getElementById('prev-btn');
-var nextBtn = document.getElementById('next-btn');
+var carousel = document.getElementById('carousel');
+var carouselInner = document.getElementById('carousel-inner');
 var titre = document.getElementById('titre');
 var filmDes = document.getElementById('description');
 var boutonInfo = document.getElementById('boutoninfo');
@@ -14,44 +12,49 @@ var fVotes = document.createElement('p');
 var annee = document.createElement('p');
 
 async function fetchMeilleursFilmsSite(url,triparvotes){
-         const response = await fetch(url+triparvotes);
-         const result = await response.json();
-         return result;
-    }
+    const response = await fetch(url+triparvotes);
+    const result = await response.json();
+    return result;
+}
 
 async function renderCarousel(){
     let url = "http://127.0.0.1:8000/api/v1/titles/";
     let triparvotes = "?sort_by=-votes,-imdb_score";    
     const datas = await fetchMeilleursFilmsSite(url,triparvotes);
     renderCarousel.innerHTML = "";
-    number = 0;
 
     for (data in datas.results){
-            console.log();
-
             image.src = datas["results"][data]['image_url'];
             image.alt = 'Image ${ data }';
-
+            image.className = "carousel-item";
             image.classList.add("active");
-            meilleurFilm.appendChild(image);    
-    }
+            carouselInner.appendChild(image);
+    }     
 }
 
-prevBtn.addEventListener("click",function(){
-    updateCarousel();
-});
 
-nextBtn.addEventListener("click",function(){
-    updateCarousel();
-});
+function updateCarousel(param){
+    if (param == 0){
+        const carouselSlider = document.querySelector("#carousel").offsetWidth;
+        const carouselContent = document.querySelector("#carousel-inner");
+        carouselInner.scrollLeft -= carouselSlider;
+        const scrollLeft = carouselContent.scrollLeft;
+        const itemsSlider = carouselInner.querySelectorAll(".carousel-item")
 
-function updateCarousel(){
-    const images = Array.from(carouselInner.children);
-    for (image in images){
-        if (index === currentIndex){
-            image.classList.add("active");
-        } else{
-            image.classList.remove("active");
+        if (scrollLeft == 0){
+            carouselContent.scrollLeft = carouselSlider * (itemsSlider.length - 1);
+        }
+    }
+
+    if(param == 1){
+        const carouselSlider = document.querySelector("#carousel").offsetWidth;
+        const carouselContent = document.querySelector("#carousel-inner");
+        carouselInner.scrollLeft += carouselSlider;
+        const scrollLeft = carouselContent.scrollLeft;
+        const itemsSlider = carouselInner.querySelectorAll(".carousel-item")
+
+        if (scrollLeft == 0){
+            carouselContent.scrollLeft = carouselSlider * (itemsSlider.length - 1);
         }
     }
 }
