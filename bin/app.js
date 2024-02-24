5d814a750ff69d3d81216fae5ca6638a4175dfea
MentalDeFer972 <joffreyterrine@protonmail.com>
2024-02-24 19:55:01 +0100

    ajoutcodecarousel

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,5 @@
 var imageFilm = document.getElementById('couverture');
-var meilleurFilm = document.getElementById('meilleur-film');
+var meilleurFilm = document.getElementById('meilleur');
 var carousel = document.getElementById('carousel');
 var carouselInner = document.getElementById('carousel-inner');
 var titre = document.getElementById('titre');
@@ -10,50 +10,32 @@ var image = document.createElement('img');
 var tFilm = document.createElement('h1')
 var fVotes = document.createElement('p');
 var annee = document.createElement('p');
+var titreCaroussel = document.createElement('p');
 
 var position = 0;
 
-var dataList = []
-
-async function fetchMeilleursFilmsSite(url,triparvotes){
+async function fetchMeilleursFilmsSite(){
+    let url = "http://127.0.0.1:8000/api/v1/titles/";
+    let triparvotes = "?sort_by=-votes,-imdb_score"; 
     const response = await fetch(url+triparvotes);
     const result = await response.json();
     return result;
 }
 
+
 async function renderCarousel(number){
-    let url = "http://127.0.0.1:8000/api/v1/titles/";
-    let triparvotes = "?sort_by=-votes,-imdb_score";    
-    dataList = await fetchMeilleursFilmsSite(url,triparvotes);
 
-    console.log(dataList);
+        dataList = await fetchMeilleursFilmsSite();
+        
+        titreCaroussel.textContent = "Films les mieux notés";
 
-    image.src = dataList["results"][number]['image_url'];
-    image.alt = 'Image'+number;
-    image.className = "carousel-item";
-    image.classList.add("active");
-    carouselInner.appendChild(image);   
+        dataList.results.slice(number,number+dataList['results'].length).forEach(item =>{
+            const image = document.createElement('img');
+            image.src = item.image_url;
+            image.alt = "Image";
+            image.className = "image-carousel";
+            carouselInner.appendChild(image);
+        });
 }
 
 renderCarousel(0);
-
-function updateCarousel(param){
-
-    if (param == 0){
-            position--;
-            renderCarousel(position);
-        }
-    if(param == 1){
-        if (position < 0 || position > dataList['results'].length){
-            position = 0;
-        }else{
-            position++;
-            renderCarousel(position);
-        }
-    }
-
-}
-
-
-
-
