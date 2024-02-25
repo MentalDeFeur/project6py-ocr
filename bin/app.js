9e3f9eda39b54c095a35faf4238a09169e5cae52
MentalDeFer972 <joffreyterrine@protonmail.com>
2024-02-25 12:05:10 +0100

    addcarrouselandmodalcode

--- bin/app.js
+++ bin/app.js
@@ -4,37 +4,61 @@ var carousel = document.getElementById('carousel');
 var carouselInner = document.getElementById('carousel-inner');
 var filmDes = document.getElementById('description');
 var boutonInfo = document.getElementById('boutoninfo');
-
-var image = document.createElement('img');
-var tFilm = document.createElement('h1')
-var fVotes = document.createElement('p');
-var annee = document.createElement('p');
-
 var position = 0;
 
+let url = "http://127.0.0.1:8000/api/v1/titles/";
+
 async function fetchMeilleursFilmsSite(){
-    let url = "http://127.0.0.1:8000/api/v1/titles/";
     let triparvotes = "?sort_by=-votes,-imdb_score"; 
     const response = await fetch(url+triparvotes);
     const result = await response.json();
     return result;
 }
 
-async function renderCarousel(number){
-
+async function renderCarousel(){
         dataList = await fetchMeilleursFilmsSite();
 
         console.log(dataList);
 
-        dataList.results.slice(number,number+dataList['results'].length).forEach(item =>{
-            
+        let number = 0;
+
+        for (data of dataList['results']){
             const image = document.createElement('img');
-            image.src = item.image_url;
+            image.src = data.image_url;
             image.alt = "Image";
             image.className = "image-carousel";
-            image.setAttribute('onClick','ouvertureModal()');
+            image.setAttribute('onClick',`executeModal("${dataList["results"][number]["id"]}")`)
             carouselInner.appendChild(image);
-        });
+            number = number+1;
+        }  
 }
 
-renderCarousel(0);
+function executeModal(id){
+    let modal = document.getElementById("modal");
+    let span = document.getElementsByClassName("close");
+    fetchModalData(id);
+
+    modal.style.display = "block";
+
+    span.onclick = function(){
+        modal.style.display = "none";
+    }
+}
+
+async function fetchModalData(id){
+
+    const response = await fetch(url+id);
+    const result = await response.json();
+    
+    document.getElementById("cover").src = result["image_url"];
+    document.getElementById("title").innerHTML = result["title"];
+    document.getElementById("year").innerHTML = result["year"];
+    document.getElementById("duration").innerHTML = result["duration"];
+    document.getElementById("genres").innerHTML = result["genres"];
+    document.getElementById("imdb").innerHTML = result["imdb_score"]+" / 10";
+    document.getElementById("director").innerHTML = result["actors"]+ "...";
+    document.getElementById("country").innerHTML = result["countries"];
+    document.getElementById("rating").innerHTML = result["rated"];
+}
+
+renderCarousel();
