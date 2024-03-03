d3a8074ec1888f12e165f0eb006dda054b099fc5
MentalDeFer972 <joffreyterrine@protonmail.com>
2024-03-03 10:43:16 +0100

    ajoutcodeautrecategorie

--- bin/app.js
+++ bin/app.js
@@ -1,7 +1,20 @@
 var imageFilm = document.getElementById('couverture');
 var meilleurFilm = document.getElementById('meilleur');
 var carousel = document.getElementById('carousel');
+
+var categorie1 = document.getElementById('categorie1');
+var carousel1 = document.getElementById('carousel1');
+
+var categorie2 = document.getElementById('categorie2');
+var carousel2 = document.getElementById('carousel2');
+
+var categorie3 = document.getElementById('categorie3');
+var carousel3 = document.getElementById('carousel3');
+
 var carouselInner = document.getElementById('carousel-inner');
+var carouselInner2 = document.getElementById('carousel-inner1');
+var carouselInner3 = document.getElementById('carousel-inner2');
+var carouselInner4 = document.getElementById('carousel-inner3');
 var filmDes = document.getElementById('description');
 var boutonInfo = document.getElementById('boutoninfo');
 var position = 0;
@@ -15,26 +28,96 @@ async function fetchMeilleursFilmsSite(){
     return result;
 }
 
+async function fetchCategorieOneSite(){
+    const response = await fetch("http://127.0.0.1:8000/api/v1/titles/?genre=drama&sort_by=-imdb_score");
+    const result = await response.json();
+    return result;
+}
+
+async function fetchCategorieTwoSite(){
+    const response = await fetch("http://127.0.0.1:8000/api/v1/titles/?genre=romance&sort_by=-imdb_score");
+    const result = await response.json();
+    return result;
+}
+
+async function fetchCategorieThreeSite(){
+    const response = await fetch("http://127.0.0.1:8000/api/v1/titles/?genre=history&sort_by=-imdb_score");
+    const result = await response.json();
+    return result;
+}
+
 async function renderCarousel(){
         dataList = await fetchMeilleursFilmsSite();
-        console.log(dataList);
-        let number = 0;
+        dataList2 = await fetchCategorieOneSite();
+        dataList3 = await fetchCategorieTwoSite();
+        dataList4 = await fetchCategorieThreeSite();
+
+
+        var number = 0;
 
         const h2 = document.createElement('h2');
         h2.textContent = "Films les mieux notés";
         meilleurFilm.parentNode.insertBefore(h2,meilleurFilm);
 
-        
 
         for (data of dataList['results']){
             const image = document.createElement('img');
             image.src = data.image_url;
             image.alt = "Image";
             image.className = "image-carousel";
-            image.setAttribute('onClick',`executeModal("${dataList["results"][number]["id"]}")`)
+            image.setAttribute('onClick',`executeModal("${dataList["results"][number]["id"]}")`);
             carouselInner.appendChild(image);
             number = number+1;
         }  
+
+        number = 0;
+
+        const h22 = document.createElement('h2');
+        h22.textContent = "Drama";
+        categorie1.parentNode.insertBefore(h22,categorie1);
+
+        for (data of dataList2['results']){
+            const image = document.createElement('img');
+            image.src = data.image_url;
+            image.alt = "Image";
+            image.className = "image-carousel2";
+            image.setAttribute('onClick',`executeModal("${dataList2["results"][number]["id"]}")`);
+            carouselInner2.appendChild(image);
+            number = number+1;
+        }  
+
+        number = 0;
+
+        const h23 = document.createElement('h2');
+        h23.textContent = "Romance";
+        categorie2.parentNode.insertBefore(h23,categorie2);
+
+        for (data of dataList3['results']){
+            const image = document.createElement('img');
+            image.src = data.image_url;
+            image.alt = "Image";
+            image.className = "image-carousel3";
+            image.setAttribute('onClick',`executeModal("${dataList3["results"][number]["id"]}")`);
+            carouselInner3.appendChild(image);
+            number = number+1;
+        }  
+
+        number = 0;
+
+        const h24 = document.createElement('h2');
+        h24.textContent = "History";
+        categorie3.parentNode.insertBefore(h24,categorie3);
+
+        for (data of dataList4['results']){
+            const image = document.createElement('img');
+            image.src = data.image_url;
+            image.alt = "Image";
+            image.className = "image-carousel4";
+            image.setAttribute('onClick',`executeModal("${dataList4["results"][number]["id"]}")`);
+            carouselInner4.appendChild(image);
+            number = number+1;
+        } 
+
 }
 
 function executeModal(id){
@@ -53,7 +136,7 @@ async function fetchModalData(id){
 
     const response = await fetch(url+id);
     const result = await response.json();
-    
+
     document.getElementById("director").textContent = result["directors"];
     document.getElementById("cover").src = result["image_url"];
     document.getElementById("title").textContent = result["title"];
